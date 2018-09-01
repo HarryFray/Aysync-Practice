410dcb8bcb3c7989ffa3da694d10dc93f6588820
HarryFray <harry.fray7@gmail.com>
2018-09-01 11:49:07 -0500

    REST api set up for github

--- src/components/App.js
+++ src/components/App.js
@@ -10,11 +10,28 @@ class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://localhost:8080/').then(res => console.log(res));
+    axios
+      .get('https://api.github.com/rate_limit')
+      .then(res => console.log(res));
+    axios
+      .get(
+        `https://api.github.com/search/repositories?q=stars:>1+language:${'python'}&sort=stars&order=desc&type=Repositories`
+      )
+      .then(res => {
+        this.setState({
+          repos: res.data.items
+        });
+      });
   }
 
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <ul>
+        {this.state.repos.map(repo => {
+          return <li key={repo.name}>{repo.name}</li>;
+        })}
+      </ul>
+    );
   }
 }
 
